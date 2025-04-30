52a136f3cb66fd74123995560b6281cf6247f2a6
Jahid Hasan <mdminhajulislamshobuj@gmail.com>
2025-04-30 11:44:06 +0600

    added layout file

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>this is home</h1>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <>
+      <h1>This is Register form</h1>
+    </>
+  );
+};
+
+export default Register;
