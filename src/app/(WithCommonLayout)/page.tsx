e8312ba5287ec6334da4044ecf0c04ce5770cca7
marufhossain55 <marufhossain55@gmail.com>
2025-05-01 16:28:03 +0600

    Home:SiteBanner added

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,10 +1,14 @@
+import ReviewBanner from '@/components/home/ReviewBanner';
 import SearchHero from '@/components/home/SearchHero';
+import SiteBanner from '@/components/home/SiteBanner';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <SearchHero />
+      <ReviewBanner />
+      <SiteBanner />
     </div>
   );
 };
