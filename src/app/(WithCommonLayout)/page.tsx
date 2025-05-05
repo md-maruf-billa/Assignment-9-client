dae34e5e2ec7babfe7b2e024522c180ed2a0fbb0
Md Imam Hosen <mimam22.cse@bu.ac.bd>
2025-05-06 03:08:01 +0600

    feat: add categroy slider for homepage

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -1,5 +1,5 @@
 import BusinessPromo from "@/components/home/BusinessPromo";
-import CategorySlider from "@/components/home/CategorySlider";
+import CategorySlider, { ICategory } from "@/components/home/CategorySlider";
 import RecentReview from "@/components/home/RecentReview";
 import ReviewBanner from "@/components/home/ReviewBanner";
 import SearchHero from "@/components/home/SearchHero";
@@ -7,15 +7,17 @@ import SiteBanner from "@/components/home/SiteBanner";
 import SiteComparison from "@/components/home/SiteComparison";
 import SiteReview from "@/components/home/SiteReview";
 import WriteReviewBanner from "@/components/home/WriteReviewBanner";
+import { allCategory } from "@/services/category";
 import React from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const { data: categories } = await allCategory();
   return (
     <main>
       <SearchHero />
       <div className="container mx-auto space-y-7">
         <WriteReviewBanner />
-        <CategorySlider />
+        <CategorySlider {...(categories as ICategory[])} />
         <BusinessPromo />
         <SiteReview />
         <ReviewBanner />
